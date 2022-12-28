4fa4d567b2569b2099c0932a2074e9f97426159a
Rohan Bansal <rohan@parsimony.com>
2022-12-28 12:20:19 +0530

    feat: initial commit

--- cloud_storage/config/desktop.py
+++ cloud_storage/config/desktop.py
@@ -1,10 +1,5 @@
 from frappe import _
 
+
 def get_data():
-	return [
-		{
-			"module_name": "Cloud Storage",
-			"type": "module",
-			"label": _("Cloud Storage")
-		}
-	]
+	return [{"module_name": "Cloud Storage", "type": "module", "label": _("Cloud Storage")}]
--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
-from setuptools import setup, find_packages
+# TODO: Remove this file when bench >=v5.11.0 is adopted / v15.0.0 is released
+from setuptools import setup
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
+name = "cloud_storage"
 
-# get version from __version__ variable in cloud_storage/__init__.py
-from cloud_storage import __version__ as version
-
-setup(
-	name="cloud_storage",
-	version=version,
-	description="Frappe App for integrating with cloud storage applications",
-	author="Agritheory",
-	author_email="developers@agritheory.dev",
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
-)
+setup()
