f72ade2edff2135f6a05709ad2fcf21dbc900d2a
agritheory <agritheory@users.noreply.github.com>
2023-02-15 19:28:30 +0000

    style: prettify code

--- cloud_storage/public/js/file.js
+++ cloud_storage/public/js/file.js
@@ -1,18 +1,19 @@
-frappe.ui.form.on("File", {
+frappe.ui.form.on('File', {
 	refresh: frm => {
 		if (!frm.doc.is_folder) {
 			// add download button
-			frm.add_custom_button(__("Get Sharing Link", "Share"), () => get_sharing_link(frm))
-			if(frm.doc.sharing_link){
-				frm.add_custom_button(__("Reset Sharing Link", "Share"), () => get_sharing_link(frm, true))
+			frm.add_custom_button(__('Get Sharing Link', 'Share'), () => get_sharing_link(frm))
+			if (frm.doc.sharing_link) {
+				frm.add_custom_button(__('Reset Sharing Link', 'Share'), () => get_sharing_link(frm, true))
 			}
 		}
-	}
+	},
 })
 
-function get_sharing_link(frm){
-	frappe.xcall('cloud_storage.cloud_storage.overrides.file.get_sharing_link', {docname: frm.doc.name, reset: true})
-	.then(r => { 
-		frappe.msgprint(r, __('Sharing Link'))
-	})
+function get_sharing_link(frm) {
+	frappe
+		.xcall('cloud_storage.cloud_storage.overrides.file.get_sharing_link', { docname: frm.doc.name, reset: true })
+		.then(r => {
+			frappe.msgprint(r, __('Sharing Link'))
+		})
 }
