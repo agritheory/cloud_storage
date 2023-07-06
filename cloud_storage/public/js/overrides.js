922cfeabc2bae2e930aa4b94d5f812472b70fa0a
Tyler Matteson <tyler@agritheory.com>
2023-07-06 07:58:46 -0400

    wip: override upload dialog

--- cloud_storage/public/js/overrides.js
+++ cloud_storage/public/js/overrides.js
@@ -1,3 +1,7 @@
+frappe.provide('frappe.ui')
+
+import { Attachments } from '../../../../frappe/frappe/public/js/frappe/form/sidebar/attachments'
+
 $(window).on('hashchange', page_changed)
 $(window).on('load', page_changed)
 
@@ -22,3 +26,39 @@ function disallow_attachment_delete(frm) {
 		frm.$wrapper.find('.attachment-row').find('.remove-btn').hide()
 	}
 }
+
+class CloudStorageAttachments extends Attachments {
+	constructor(opts) {
+		super()
+	}
+	new_attachment(fieldname) {
+		if (this.dialog) {
+			// remove upload dialog
+			this.dialog.$wrapper.remove()
+		}
+		console.log('new attachment')
+		const restrictions = {}
+		if (this.frm.meta.max_attachments) {
+			restrictions.max_number_of_files = this.frm.meta.max_attachments - this.frm.attachments.get_attachments().length
+		}
+
+		new frappe.ui.FileUploader({
+			doctype: this.frm.doctype,
+			docname: this.frm.docname,
+			frm: this.frm,
+			folder: 'Home/Attachments',
+			on_success: async file_doc => {
+				await this.version_check()
+				this.attachment_uploaded(file_doc)
+			},
+			restrictions,
+			make_attachments_public: this.frm.meta.make_attachments_public,
+		})
+	}
+	async version_check() {
+		console.log('version check')
+		return
+	}
+}
+
+frappe.ui.form.Attachments = CloudStorageAttachments
