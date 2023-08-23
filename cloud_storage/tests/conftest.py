c24e5aed10277fc8e296e633f2fc52337dafb115
Tyler Matteson <tyler@agritheory.com>
2023-08-23 08:17:28 -0400

    test: disable db logger in test

--- cloud_storage/tests/conftest.py
+++ cloud_storage/tests/conftest.py
@@ -1,10 +1,25 @@
-import pytest
-import frappe
-from unittest.mock import MagicMock, patch
-from frappe.defaults import *
 import os
 from pathlib import Path
+from unittest.mock import MagicMock, patch
+
+import frappe
+import pytest
+from frappe.defaults import *
 from frappe.utils import get_bench_path
+
+
+def _get_logger(*args, **kwargs):
+	from frappe.utils.logger import get_logger
+
+	return get_logger(
+		module=None,
+		with_more_info=False,
+		allow_site=True,
+		filter=None,
+		max_size=100_000,
+		file_count=20,
+		stream_only=True,
+	)
 
 
 @pytest.fixture(scope="module")
@@ -15,6 +30,8 @@ def monkeymodule():
 
 @pytest.fixture(scope="session", autouse=True)
 def db_instance():
+	frappe.logger = _get_logger
+
 	currentsite = "test_site"
 	sites = Path(get_bench_path()) / "sites"
 	if (sites / "currentsite.txt").is_file():
