83089310650fe7b54e829584ca71773c6b9b7602
Rohan Bansal <rohan@parsimony.com>
2024-02-20 18:19:24 +0530

    fix: avoid name conflicts during file upload

--- cloud_storage/cloud_storage/overrides/file.py
+++ cloud_storage/cloud_storage/overrides/file.py
@@ -394,7 +394,7 @@ def upload_file(file: File) -> File:
 	except Exception as e:
 		frappe.log_error("File Upload Error", e)
 	file.db_set("s3_key", path)
-	if not file.name:
+	if frappe.flags.cloud_storage and not file.name:
 		file.save()
 	return file
 
