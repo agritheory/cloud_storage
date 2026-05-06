d7d39261de6a72de39e549798f071f3b67f40a51
Tyler Matteson <tyler@agritheory.com>
2026-05-06 11:57:05 -0400

    chore: migrate to version-16
    Bumped version to 16.0.0

--- cloud_storage/__init__.py
+++ cloud_storage/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, AgriTheory and contributors
 # For license information, please see license.txt
 
-__version__ = "15.7.2"
+__version__ = "16.0.0"
 
 
 import frappe.desk.form.load
