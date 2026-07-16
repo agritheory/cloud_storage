5b7c8254c63578287aee3127dda6b4bfdae01132
lauty95 <lautaroj95@gmail.com>
2026-07-16 16:30:46 +0000

    feat: add Local File Cache doctype

---
+++ cloud_storage/cloud_storage/doctype/local_file_cache/local_file_cache.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, AgriTheory and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Local File Cache", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ cloud_storage/cloud_storage/doctype/local_file_cache/test_local_file_cache.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, AgriTheory and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLocalFileCache(FrappeTestCase):
+	pass
