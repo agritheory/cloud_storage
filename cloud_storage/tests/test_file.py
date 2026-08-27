a97ad6e10ec59a12f6902f70ee05f89b0bcdf81f
lauty95 <lautaroj95@gmail.com>
2026-08-27 17:16:34 +0000

    fix: linters

--- cloud_storage/tests/test_file.py
+++ cloud_storage/tests/test_file.py
@@ -292,7 +292,6 @@ def test_associate_files_no_duplicate_association():
 	doc.associate_files("Module Def", "Cloud Storage")
 	doc.associate_files("Module Def", "Cloud Storage")
 
-
 	file_b = frappe.get_doc("File", "test-assoc-guard-b")
 	matching = [
 		a
